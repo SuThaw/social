2fafd51c588995b2bdc236323ba0905acba307cb
Su Thaw <suthaw524@gmail.com>
2015-06-20 12:43:26 +0800

    API finished

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var Post = require('./models/post');
 var app = express();
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-app.get('/api/posts',function(req,res) {
-	res.json({
-		username:'dickeyxxx',
-		body:'node rocks'
+app.get('/api/posts',function(req,res,next) {
+	Post.find(function(err,posts){
+		if(err) return next(err);
+		res.json(posts);
 	});
 });
+
+app.post('/api/posts',function(req,res,next){
+//	console.log(req.body);
+	var post = new Post({
+		username:req.body.username,
+		body:req.body.body
+	});
+	post.save(function(err,post){
+		if(err){ return next(err);}
+		res.json(201,post);
+	});
+
+});
+
 app.listen(3000,function(){
 	console.log('serve listening on',3000);
 });
