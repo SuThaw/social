2f2ef5b053189e77cd1292cd42fb8311c9acd6d8
Su Thaw <suthaw524@gmail.com>
2015-06-20 14:53:02 +0800

    separate api to controllers

--- server.js
+++ server.js
@@ -1,36 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var Post = require('./models/post');
 var app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+app.use('/api/posts',require('./controllers/api/posts'));
+app.use(require('./controllers/static.js'));
 
 app.get('/',function(req,res){
 	res.sendfile('layouts/posts.html');
 });
 
-app.get('/api/posts',function(req,res,next) {
-	Post
-	.find()
-	.sort('-date')
-	.exec(function(err,posts){
-		if(err) return next(err);
-		res.json(posts);
-	});
-});
 
-app.post('/api/posts',function(req,res,next){
-//	console.log(req.body);
-	var post = new Post({
-		username:req.body.username,
-		body:req.body.body
-	});
-	post.save(function(err,post){
-		if(err){ return next(err);}
-		res.json(201,post);
-	});
-
-});
 
 app.listen(3000,function(){
 	console.log('serve listening on',3000);
