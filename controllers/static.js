6a92cbaca8e3ca818f5e62edf486a2c70b16c3f3
Su Thaw <suthaw524@gmail.com>
2015-06-21 08:37:19 +0800

    Separate Angular js into service

--- controllers/static.js
+++ controllers/static.js
@@ -1,5 +1,9 @@
-var router = require('express').Router();
+var express = require('express');
+var router = express.Router();
 var path = require('path');
+router.use(express.static(__dirname + '/../assets'));
+
+
 router.get('/',function(req,res){
 	res.sendFile(path.join(__dirname,'../layouts','posts.html'));
 });
