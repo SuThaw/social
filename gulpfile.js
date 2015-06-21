d908267fbec4d2e4a6677b0fad4c3535fb12f468
Su Thaw <suthaw524@gmail.com>
2015-06-21 11:15:11 +0800

    Separe gulp task inot module and add css stylus

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,20 @@ var uglify = require('gulp-uglify');
 var ngAnnotate = require('gulp-ng-annotate');
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('js',function(){
-	gulp
-		.src(['ng/module.js','ng/**/*.js'])
-		.pipe(concat('app.js'))
-		.pipe(ngAnnotate())
-		.pipe(uglify())
-		.pipe(sourcemaps.writer())
-		.pipe(gulp.dest('assets'))
+var fs = require('fs');
+
+fs
+	.readdirSync(__dirname + '/gulp')
+	.forEach(function(task){
+		require('./gulp/' + task);
+	});
+
 		
-});
 
 gulp.task('watch:js',['js'],function(){
 	gulp.watch('ng/**/*.js',['js'])
+});
+
+gulp.task('watch:css',function(){
+	gulp.watch('css/**/*.styl',['css']);
 });
