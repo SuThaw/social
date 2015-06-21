ee4d55c475ca677a60590444002267833558f581
Su Thaw <suthaw524@gmail.com>
2015-06-21 10:06:48 +0800

    Add Glup task and separate angular code

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,5 @@
+angular.module('app',[]);
+
 var app =  angular.module('app',[]);
 app.service('PostsSvc',function($http){
 	this.fetch = function(){
@@ -30,3 +32,38 @@ app.controller('PostsCtrl',function($scope,PostsSvc){
 			$scope.posts = posts;
 		})
 });
+
+angular
+	.module('app')
+	.controller('PostsCtrl',function($scope,PostsSvc){
+		$scope.addPost = function() {
+			if($scope.postBody){
+				PostsSvc
+					.create({
+						username:'SuThaw',
+						body:$scope.postBody
+					})
+					.success(function(post) {
+						$scope.posts.unshift(post);
+						$scope.postBody = null
+					});
+			}
+		};
+		PostsSvc
+			.fetch()
+			.success(function (posts) {
+				$scope.posts = posts;
+			})
+	});
+
+angular
+	.module('app')
+	.service('PostsSvc',function($http){
+		this.fetch = function(){
+			return $http.get('/api/posts');
+		}
+
+		this.create = function(post){
+			return $http.post('/api/posts',post);
+		}
+	});
