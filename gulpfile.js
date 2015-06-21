eb4e572f779d38291b73b83fa6ffad1c0c66601d
Su Thaw <suthaw524@gmail.com>
2015-06-21 10:28:28 +0800

    Add Gulp ng-annotate and ng source map

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-
+var uglify = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('js',function(){
 	gulp
 		.src(['ng/module.js','ng/**/*.js'])
 		.pipe(concat('app.js'))
+		.pipe(ngAnnotate())
+		.pipe(uglify())
+		.pipe(sourcemaps.writer())
 		.pipe(gulp.dest('assets'))
 		
+});
+
+gulp.task('watch:js',['js'],function(){
+	gulp.watch('ng/**/*.js',['js'])
 });
