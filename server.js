0377fc274d2302f11a7348a2cc6e059d06cde669
Su Thaw <suthaw524@gmail.com>
2015-06-24 01:06:27 +0800

    Add auth middleware

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
+app.use(require('./auth'));
 app.use('/api/posts',require('./controllers/api/posts'));
+
+
 
 app.use('/api/sessions',require('./controllers/api/sessions'));
 app.use('/api/users',require('./controllers/api/users'));
-
 app.use(require('./controllers/static.js'));
 
 app.get('/',function(req,res){
