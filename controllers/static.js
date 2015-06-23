95bb869122c3ccd86933a3955f386faab24038dc
Su Thaw <suthaw524@gmail.com>
2015-06-23 19:07:18 +0800

    Create frontend for login

--- controllers/static.js
+++ controllers/static.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 router.use(express.static(__dirname + '/../assets'));
-
+router.use(express.static(__dirname + '/../templates'));
 
 router.get('/',function(req,res){
-	res.sendFile(path.join(__dirname,'../layouts','posts.html'));
+	res.sendFile(path.join(__dirname,'../layouts','app.html'));
 });
 
 module.exports = router;
