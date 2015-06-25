3f7c9026c0eb92766011a5917224c57a2616e3d9
Su Thaw <suthaw524@gmail.com>
2015-06-25 11:33:32 +0800

    Add auto reconnect web socket

--- ng/websockets.js
+++ ng/websockets.js
@@ -1,16 +1,24 @@
 angular
 	.module('app')
-	.run(function($rootScope){
-		var url = "ws://localhost:3000";
-		var connection = new WebSocket(url);
-
+	.run(function($rootScope,$timeout){
+		(function connect(){
+			var url = "ws://localhost:3000";
+			var connection = new WebSocket(url);
+			connection.onclose = function(e){
+				console.log('Websocket closed. Reconnecting ...');
+				$timeout(connect,10*1000);
+			};
+			
 		connection.onopen = function(){
 			console.log('Web Socket connected');
-		};
-		connection.onmessage = function(e){
-			console.log(e);
-			var payload = JSON.parse(e.data);
-			$rootScope.$broadcast('ws:'+ payload.topic, payload.data);
 		}
+			connection.onmessage = function(e){
+				console.log(e);
+				var payload = JSON.parse(e.data);
+				$rootScope.$broadcast('ws:'+ payload.topic, payload.data);
+			}
 
+		
+		})();
+		
 	});
