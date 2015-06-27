7638854a38f6f3f31451673080a8d85056b8b2ca
Su Thaw <suthaw524@gmail.com>
2015-06-27 11:50:17 +0800

    Add js hint and code coverage

--- test/server/controllers/api/posts.spec.js
+++ test/server/controllers/api/posts.spec.js
@@ -1,10 +1,58 @@
 var expect = require('chai').expect;
+var api = require('../../support/api');
+var user = require('../../support/user');
+var Post = require('../../../../models/post');
 
-var ctrl = require('../../../../controllers/api/posts');
+describe('controllers.api.posts',function(){
+	describe('GET /api/posts',function(){
+		beforeEach(function(done){
+			Post.remove({},done);
 
-describe('controllers.api.post',function  () {
-	// body...
-	it('exists',function(){
-		expect(ctrl).to.exists;
+		});
+
+		describe('GET /api/postss',function(){
+		
+			beforeEach(function(done){
+				var posts = [{username:'Suthaw',body:'Post 1'},{username:'TLH',body:'Post 2'},{username:'sumyat',body:'Post 3'}];
+				Post.create(posts,done);
+			});	
+
+			it('has 3 posts',function(done){
+				api
+					.get('/api/posts')
+					.expect(200)
+					.expect(function(response){
+						expect(response.body).to.have.length(3);
+					})
+					.end(done);
+			});
+		});
+
+		describe('GET /api/posts',function(){
+			var token;
+			beforeEach(function(done){
+				user.create('suthaw','plp2tlh',function(err,user){
+					token = user.token;
+					done(err);
+				})	
+			});
+
+			beforeEach(function(done){
+				api
+					.post('/api/posts')	
+					.send({body:'this is my new post'})
+					.set('x-Auth',token)
+					.expect(201)
+					.end(done)
+			});
+
+			it('added 1 new post',function(done){
+				Post.findOne(function(err,post){
+					expect(post.body).to.equal('this is my new post');
+					done(err);
+					
+				});
+			});
+		});
 	});
-})
+});
